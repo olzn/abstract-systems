472a66cab24aaf27373a40249aa9166912d77a82
Oscar Lozano <contact@olzn.co>
2020-07-23 15:42:29 +0100

    easter egg

---
+++ script.mjs
@@ -0,0 +1,35 @@
+var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var current = 0;
+
+var keyHandler = function (event) {
+
+	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+
+	// Update how much of the pattern is complete
+	current++;
+
+	// If complete, alert and reset
+	if (pattern.length === current) {
+		current = 0;
+		elementAnimate();
+	}
+
+};
+
+function elementAnimate(){
+	document.getElementById("sanic").animate([
+		// keyframes
+		{ transform: 'translateX(0px)' }, 
+		{ transform: 'translateX(150vw)' }
+	  ], { 
+		// timing options
+		duration: 3000,
+	  });
+}
+
+// Listen for keydown events
+document.addEventListener('keydown', keyHandler, false);
